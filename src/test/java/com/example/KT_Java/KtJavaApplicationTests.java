c3f238035eda36c0b121e6f498dcd11e0713003f
Nick <jinvo2311@gmail.com>
2023-06-01 22:28:53 +0700

    Git

---
+++ src/test/java/com/example/KT_Java/KtJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.KT_Java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KtJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
