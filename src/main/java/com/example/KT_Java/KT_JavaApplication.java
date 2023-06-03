488ed11ecb282f19be35958f5d9d9f654dda5310
NNick311 <jinvo2311@gmail.com>
2023-06-03 21:01:29 +0700

    Code

---
+++ src/main/java/com/example/KT_Java/KT_JavaApplication.java
@@ -0,0 +1,14 @@
+package com.example.KT_Java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KT_JavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KT_JavaApplication.class, args);
+	}
+
+
+}
